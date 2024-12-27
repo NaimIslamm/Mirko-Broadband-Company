4fe18f55c634cf64620c4fca3b4b3ced925e28af
NAIM ISLAM <naimmislam@gmail.com>
2024-12-28 01:36:22 +0600

    added scroll-effect

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ new Swiper(".carousel", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// scroll effect
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  }
+});
